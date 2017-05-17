b80fdfd6eebdba6503c8e53617b6969b49d663c1
Nikita <nukenukem@yandex.ru>
2017-05-17 22:51:16 +0200

    continuance worл

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
  window.onload = function() {
 
+var head = document.getElementsByClassName("head");
 
-var menu = document.getElementById('menu');
-var textMenu = document.getElementById("textMenu");
-var close = document.getElementById("close");
+for(var i = 0; i < head.length; i++) {
+   	head[i].onclick = function(e) {
+    var head = e.target;
 
-function openMenu () {
-	textMenu.classList.toggle("open");
+   while (head != this) {
+    head = head.parentNode;
+    if (head.className == 'head') {
+    	var panel = head.parentNode;
+ 		panel.classList.toggle("hide");
+        return;
+   		}
+ 	}
+}
+
 };
-
-menu.onclick = openMenu;
-close.onclick = openMenu;
-
- };
-
+};
