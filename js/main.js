52b09ea1795b80c1c46ff552be641287ddc058f9
Nikita <nukenukem@yandex.ru>
2017-05-20 01:52:20 +0200

    Add slider. Only Native js

--- js/main.js
+++ js/main.js
@@ -15,6 +15,41 @@ for(var i = 0; i < head.length; i++) {
    		}
  	}
 }
-
 };
+
+var bottomLeft = document.getElementsByClassName("fa-chevron-left");
+var bottomRight = document.getElementsByClassName("fa-chevron-right");
+var currentSlide = 0;
+
+for(var i = 0; i < bottomLeft.length; i++) {
+   	bottomLeft[i].onclick = function(e) {
+   	var slideContainer = e.target.parentNode;
+   	var slide = slideContainer.getElementsByClassName("slide");
+   	previousSlide();
+   	function previousSlide(){
+		goToSlide(currentSlide-1);
+			}
+	function goToSlide(n){
+		slide[currentSlide].classList.toggle("hide");
+		currentSlide = (n+slide.length)%slide.length;
+		slide[currentSlide].classList.toggle("hide");
+		}
+		};
+};
+
+for(var i = 0; i < bottomRight.length; i++) {
+   	bottomRight[i].onclick = function(e) {
+   	var slideContainer = e.target.parentNode;
+   	var slide = slideContainer.getElementsByClassName("slide");
+   	 nextSlide();
+   	function nextSlide(){
+		goToSlide(currentSlide+1);
+	}
+	function goToSlide(n){
+		slide[currentSlide].classList.toggle("hide");
+		currentSlide = (n+slide.length)%slide.length;
+		slide[currentSlide].classList.toggle("hide");
+		}
+		};
+	};
 };
